38ac12ed52fd270372de0c706f87bee6c3b8d3be
Alex <alex.zeiss@gmx.net>
2024-06-14 16:34:42 +0200

    Add eslint

---
+++ server/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tsEslint.configs.recommended,
+];
